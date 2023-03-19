492d4716617ccbdd0e7fd9b2c9cf51c20ad34d3a
Mark K Shumway <mark.shumway@swanriver.dev>
2023-03-18 20:14:48 -0400

    Example task submission to the Sentinel

---
+++ sentinel/sentinel/sentinel_task.py
@@ -0,0 +1,31 @@
+# sentinel_task.py 
+
+import argparse
+import json
+import zmq
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--date", required=True, help="Date (YYYY-MM-DD)")
+ap.add_argument("-e", "--event", help="Event ID")
+ap.add_argument("-t", "--task", default="MobileNetSSD_allFrames", help="Task name")
+args = vars(ap.parse_args())
+event_date = args["date"]
+event_id = args["event"]
+task_id = args["task"]
+
+# Example task submission to the Sentinel
+
+request = {'sink': 'data1', 
+           'node': 'testMonster',
+           'date': event_date, 
+           'event': event_id,
+           'pump': 'tcp://data1:5556', 
+           'task': task_id}
+
+msg = json.dumps(request)
+
+with zmq.Context().socket(zmq.REQ) as sock:
+    sock.connect('tcp://sentinel:5566') 
+    sock.send(msg.encode("ascii"))
+    print(sock.recv().decode("ascii"))
+    # Submission request returns 'OK', or 'Error' if submission failed
