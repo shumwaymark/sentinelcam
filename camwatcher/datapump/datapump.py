ddc109a342dfc7ff7023395080d1868f520d12ca
Mark K Shumway <mark.shumway@swanriver.dev>
2026-02-14 08:51:03 -0600

    Change event deleting logging message to DEBUG

--- camwatcher/datapump/datapump.py
+++ camwatcher/datapump/datapump.py
@@ -3,7 +3,7 @@ import os
 import json
 import logging
 import logging.config
-import pickle 
+import pickle
 import zlib
 import zmq
 import numpy as np
@@ -21,7 +21,7 @@ from sentinelcam.utils import readConfig
 # are running Python 3.8+ (Debian Bullseye or later).
 
 class DataPump(imagezmq.ImageHub):
-    """ Service access requests to camwatcher data store and Sentinel DataFeed 
+    """ Service access requests to camwatcher data store and Sentinel DataFeed
 
     Resident as a daemon sub-process on an ImageHub node, servicing
     access requests to the camwatcher data store and saved images.
@@ -30,7 +30,7 @@ class DataPump(imagezmq.ImageHub):
     ----------
     port : integer
         Port number for an ImageHub REP socket
-    
+
     Methods
     -------
     send_jpg(resp, jpeg)
@@ -42,24 +42,24 @@ class DataPump(imagezmq.ImageHub):
     """
 
     def send_jpg(self, resp, jpeg):
-        """Sends jpg buffer, preceded 
+        """Sends jpg buffer, preceded
 
         Parameters:
         -----------
         resp : str
             Response message, "OK" for success
         jpeg : buffer
-            bytestring containing the jpg image to send 
+            bytestring containing the jpg image to send
         """
         self.zmq_socket.send_jpg(msg=resp, jpg_buffer=jpeg, copy=False)
 
-    def send_DataFrame(self, 
+    def send_DataFrame(self,
                        msg='OK',
                        df=pandas.DataFrame(),
                        flags=0,
                        copy=False,
                        track=False):
-        """Sends a pandas.DataFrame 
+        """Sends a pandas.DataFrame
 
         Sends a pickled pandas.DataFrame as the response using pickle protocol 4
         for compatibility with legacy nodes running Python 3.7 and older pandas versions.
@@ -67,11 +67,11 @@ class DataPump(imagezmq.ImageHub):
 
         Parameters:
         -----------
-        msg : str 
+        msg : str
             response code or message
-        df : pandas.DataFrame 
+        df : pandas.DataFrame
             DataFrame to be pickled and sent in reply
-        flags : int, optional 
+        flags : int, optional
             zmq flags
         copy : bool, optional
             zmq copy flag
@@ -87,30 +87,30 @@ class DataPump(imagezmq.ImageHub):
         self.zmq_socket.send_json(md, flags | zmq.SNDMORE)
         return self.zmq_socket.send(buffer.getvalue(), flags, copy=copy, track=track)
 
-    def pickle_and_send(self, 
+    def pickle_and_send(self,
                         msg='OK',
                         obj=None,
                         protocol=4,  # Use protocol 4 for Python 3.7+ compatibility
                         flags=0,
                         copy=False,
                         track=False):
-        """Pickle and send  
+        """Pickle and send
 
         Pickle and compress an object to send as the response.
         Preceded by a response code or other text msg,
-        
+
         Uses pickle protocol 4 for compatibility with legacy nodes running
         Python 3.7 (Debian Buster) until all nodes are upgraded.
 
         Parameters:
         -----------
-        msg : str 
+        msg : str
             response code or message
         obj : data
-            object to be sent 
+            object to be sent
         protocol : int, optional
             pickling protocol (default 4 for Python 3.7+ compatibility)
-        flags : int, optional 
+        flags : int, optional
             zmq flags
         copy : bool, optional
             zmq copy flag
@@ -140,12 +140,12 @@ def main():
     camwatcher = zmq.Context.instance().socket(zmq.REQ)
     camwatcher.connect(CFG['camwatcher'])
     log.info("datapump response loop starting")
-    # TODO: Graceful shutdown / termination handling needed. 
+    # TODO: Graceful shutdown / termination handling needed.
     # Need a policy for sending meaningful response codes back to the DataFeed.
     # TODO: Need disk and data analysis with clean-up and reporting as a nightly task.
     # Will need control panel instrumentation for this as well, including perhaps charts
     # of the storage breakdown, utilization, and available capacity of the data sink.
-    while True:  
+    while True:
         msg = pump.zmq_socket.recv()
         request = msgpack.loads(msg)
         reply = 'OK'
@@ -154,12 +154,12 @@ def main():
                 if request['cmd'] == 'dat':  # retrieve list of date folders
                     pump.pickle_and_send(reply, cData.get_date_list())
                     continue
-                elif request['cmd'] == 'idx':  # retrieve event index 
+                elif request['cmd'] == 'idx':  # retrieve event index
                     cData.set_date(request['date'])
                     indx = cData.get_index()
                     pump.send_DataFrame(reply, indx)
                     continue
-                elif request['cmd'] == 'evt':  # retrieve event data 
+                elif request['cmd'] == 'evt':  # retrieve event data
                     cData.set_date(request['date'])
                     cData.set_event(request['evt'])
                     if 'trk' in request:
@@ -173,11 +173,11 @@ def main():
                     cData.set_date(request['date'])
                     cData.set_event(request['evt'])
                     image_list = cData.get_event_images()
-                    timestamps = [datetime.strptime(imageframe[-30:-4],"%Y-%m-%d_%H.%M.%S.%f") 
+                    timestamps = [datetime.strptime(imageframe[-30:-4],"%Y-%m-%d_%H.%M.%S.%f")
                         for imageframe in image_list]
                     pump.pickle_and_send(reply, timestamps)
                     continue
-                elif request['cmd'] == 'pic':  # retrieve image frame 
+                elif request['cmd'] == 'pic':  # retrieve image frame
                     jpegfile = os.path.join(CFG['imagefolder'], request['date'],
                         request['evt'] + '_' + request['frametime'] + '.jpg')
                     if os.path.exists(jpegfile):
@@ -189,7 +189,7 @@ def main():
                     pump.send_jpg(reply, jpeg)
                     continue
                 elif request['cmd'] == 'del':  # delete event data
-                    (date, event) = (request['date'], request['evt']) 
+                    (date, event) = (request['date'], request['evt'])
                     if facelist.event_locked(date, event):
                         reply = b'Locked'
                     else:
@@ -197,15 +197,15 @@ def main():
                         camwatcher_control['cmd'] = 'DelEvt'
                         camwatcher_control['date'] = date
                         camwatcher_control['event'] = event
-                        log.info(f"camwatcher send request {camwatcher_control}")
+                        log.debug(f"camwatcher send request {camwatcher_control}")
                         camwatcher.send(json.dumps(camwatcher_control).encode('ascii'))
                         reply = camwatcher.recv()
-                        log.info(f"camwatcher delete response {reply}")
-                elif request['cmd'] == 'HC':  # health checkcd 
+                        log.debug(f"camwatcher delete response {reply}")
+                elif request['cmd'] == 'HC':  # health checkcd
                     reply = b'OK'
                 else:
                     log.error(f"Unrecognized command: {str(request)}")
-                    reply = b'Error'    
+                    reply = b'Error'
             except KeyError as keyval:
                 log.error(f'Request field "{keyval}" missing for [{request["cmd"]}] command')
                 reply = b'Error'
